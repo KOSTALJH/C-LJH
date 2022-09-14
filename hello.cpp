ead2ae6d0cc9ce7a9e5d4eb93390978291715efb
Your Name <you@example.com>
2022-09-14 15:06:40 +0900

    hello

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello world" << endl;
+
+    int width;
+    char add[100];
+    cin.getline(add,100, ' ');
+    cout<<add<<endl;
+
+    
+}
