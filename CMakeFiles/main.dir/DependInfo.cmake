061bd765d385c1311023b036e1f6047cb7202a35
Your Name <you@example.com>
2022-09-29 17:43:17 +0900

    2022.09.29

---
+++ CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/project/cpp/BFS.c" "CMakeFiles/main.dir/BFS.c.o" "gcc" "CMakeFiles/main.dir/BFS.c.o.d"
+  "/home/ubuntu/project/cpp/adjList.c" "CMakeFiles/main.dir/adjList.c.o" "gcc" "CMakeFiles/main.dir/adjList.c.o.d"
+  "/home/ubuntu/project/cpp/main.c" "CMakeFiles/main.dir/main.c.o" "gcc" "CMakeFiles/main.dir/main.c.o.d"
+  "/home/ubuntu/project/cpp/stackL.c" "CMakeFiles/main.dir/stackL.c.o" "gcc" "CMakeFiles/main.dir/stackL.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/BFS.c.o"
+  "CMakeFiles/main.dir/BFS.c.o.d"
+  "CMakeFiles/main.dir/adjList.c.o"
+  "CMakeFiles/main.dir/adjList.c.o.d"
+  "CMakeFiles/main.dir/main.c.o"
+  "CMakeFiles/main.dir/main.c.o.d"
+  "CMakeFiles/main.dir/stackL.c.o"
+  "CMakeFiles/main.dir/stackL.c.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
